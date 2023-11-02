884299d07942b4052d021b454ab03d5358c400c9
Milovan <milovan.antic314@gmail.com>
2023-11-02 08:33:03 +0100

    Add achievements table and import

--- database/db_imports.sql
+++ database/db_imports.sql
@@ -6,3 +6,4 @@ INSERT INTO region (name) VALUES
 ('North America and South America');
 
 COPY capitals (id,country, capital, region_id) FROM 'capitals.csv' DELIMITER ',' CSV HEADER;
+COPY achievements (id,achievement_name, achievement_description) FROM 'achievements.csv' DELIMITER ',' CSV HEADER;

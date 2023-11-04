5bf8b4ecb87c1d27c720a5f0c376e6920c3ebe72
Milovan <milovan.antic314@gmail.com>
2023-11-04 18:15:00 +0100

    Add starting packages and load info from db

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
+import { config } from "dotenv";
+config();
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+    console.log(quiz[0]);
+  }
+  db.end();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  res.sendFile("index.html", { root: "./" });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });

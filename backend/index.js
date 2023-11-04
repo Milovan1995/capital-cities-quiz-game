ebc431c6578e380d2c8814f1665c21f7a5646e7d
Milovan <milovan.antic314@gmail.com>
2023-11-04 19:02:45 +0100

    Adapt index js further

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,21 @@ import pg from "pg";
 import { config } from "dotenv";
 config();
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 const db = new pg.Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 });
 
 db.connect();
 
 let quiz = [];
 
-db.query("SELECT * FROM capitals", (err, res) => {
+db.query("SELECT * FROM capitals;", (err, res) => {
   if (err) {
     console.error("Error executing query", err.stack);
   } else {
@@ -31,9 +31,6 @@ db.query("SELECT * FROM capitals", (err, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  res.sendFile("index.html", { root: "./" });
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

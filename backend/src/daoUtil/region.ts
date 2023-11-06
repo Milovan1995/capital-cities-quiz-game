916a2283de7d690ba08957a8bbf6a4322534b6c8
Milovan <milovan.antic314@gmail.com>
2023-11-06 23:53:22 +0100

    Add random region option for now

--- backend/src/daoUtil/region.ts
+++ backend/src/daoUtil/region.ts
@@ -1,5 +1,4 @@
 //there will be an option to choose a region eventually so i'm adding a bit of a randomising 'flavour' currently to make sure i'm including everything
 
-let regionNumberArray: number[] = [1, 2, 3, 4, 5];
-let regionNumber: number = regionNumberArray[Math.floor(Math.random() * 5)];
+let regionNumber: number = Math.floor(Math.random() * 5) + 1;
 export { regionNumber };

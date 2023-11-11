b7df97c1c0e06ce3c3fca3ff31771b2822e5a441
Milovan <milovan.antic314@gmail.com>
2023-11-11 01:01:17 +0100

    Add generic read method for db

--- backend/src/dao/dao.util.ts
+++ backend/src/dao/dao.util.ts
@@ -1,11 +1,12 @@
 import { db } from "./db.js";
 
+// INSERTING INTO DB
 async function insertIntoDb(
   param: Record<string, number | string>,
   tableName: string
 ): Promise<void> {
   try {
-    const { columns, placeholders, values } = generateQueryParams(param);
+    const { columns, placeholders, values } = generateInsertQueryParams(param);
     const sql: string = `INSERT INTO ${tableName}(${columns}) VALUES(${placeholders});`;
     const result = await db.query(sql, [...values]);
     console.log(`${tableName} saved: Values: ${[...values]}`);
@@ -15,11 +16,36 @@ async function insertIntoDb(
   }
 }
 
-function generateQueryParams(param: Record<string, number | string>) {
+function generateInsertQueryParams(param: Record<string, number | string>) {
   const columns = Object.keys(param).join(",");
   const values = Object.values(param);
   const placeholders = values.map((_, index) => `$${index + 1}`).join(",");
   return { columns, placeholders, values };
 }
 
-export { insertIntoDb };
+// READING FROM DB
+
+function generateSelectQueryParams(params: Record<string, number | string>) {
+  const conditions = Object.keys(params)
+    .map((key, index) => `${key} $${index + 1}`)
+    .join(" AND ");
+  const values = Object.values(params);
+  return { conditions, values };
+}
+
+async function readFromDb<T>(
+  param: Record<string, string>,
+  tableName: string
+): Promise<T[]> {
+  try {
+    const { conditions, values } = generateSelectQueryParams(param);
+    const sql: string = `SELECT * FROM ${tableName} WHERE ${conditions}`;
+    const result = await db.query(sql, [...values]);
+    return result.rows;
+  } catch (error) {
+    console.log(error, "error");
+    throw new Error(`Error reading from ${tableName}`);
+  }
+}
+
+export { insertIntoDb, readFromDb };

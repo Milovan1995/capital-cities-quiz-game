74bcdc334de4141c68bbc7f5f02535fc875d53b4
Milovan <milovan.antic314@gmail.com>
2023-11-12 01:09:13 +0100

    Add verify user post request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { config } from "dotenv";
-import { ICapital } from "./dao/ITables.js";
-import { getCapitals } from "./game/capitals.js";
 import { dbInfo } from "./dao/db.js";
+import { verifyUser } from "./auth/login.js";
 
 config();
 const app = express();
+app.use(express.json());
+
 const port = dbInfo.appPort;
-(async () => {
-  let quiz: ICapital[] = [];
-
-  try {
-    quiz = await getCapitals();
-    console.log(quiz);
-  } catch (error) {
-    console.error("Error getting capitals", error);
-  }
-})();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.post("/api/verify-user", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required." });
+  }
+
+  try {
+    const isUserValid = await verifyUser(username, password);
+
+    if (isUserValid) {
+      return res.json({ message: "User verified successfully." });
+    } else {
+      return res.status(401).json({ error: "Authentication failed." });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error." });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

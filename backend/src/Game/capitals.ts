fe4798aaad1bb7491844a66b01acdea3bef748de
Milovan <milovan.antic314@gmail.com>
2023-11-12 18:32:15 +0100

    Adapt getCapitals method

--- backend/src/Game/capitals.ts
+++ backend/src/Game/capitals.ts
@@ -1,12 +1,11 @@
 import { readFromDb } from "../dao/read.util.js";
 import { ICapital } from "../dao/ITables.js";
-import { regionNumber } from "../dao/region.js";
 
-async function getCapitals(): Promise<ICapital[]> {
+async function getCapitals(regionId: number): Promise<ICapital[]> {
   try {
     return await readFromDb<ICapital>(
       {
-        region_id: `${regionNumber}`,
+        region_id: `${regionId}`,
       },
       "capitals"
     );
--- backend/src/dao/region.ts
+++
@@ -1,4 +0,0 @@
-//there will be an option to choose a region eventually so i'm adding a bit of a randomising 'flavour' currently to make sure i'm including everything
-
-let regionNumber: number = Math.floor(Math.random() * 5) + 1;
-export { regionNumber };

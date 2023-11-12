a9808d7423bf235bc155a5db050c6bc279613301
Milovan <milovan.antic314@gmail.com>
2023-11-12 20:48:47 +0100

    Add inserUserFeedback method

--- backend/src/Game/users.ts
+++ backend/src/Game/users.ts
@@ -1,5 +1,6 @@
-import { IUser } from "../dao/ITables.js";
+import { IFeedback, IUser } from "../dao/ITables.js";
 import { insertIntoDb } from "../dao/create.util.js";
+import { readFromDb, readSingleValueFromTable } from "../dao/read.util.js";
 
 async function saveUserInfo(username: string, password: string) {
   const user: IUser = {
@@ -13,4 +14,33 @@ async function saveUserInfo(username: string, password: string) {
   }
 }
 
-export { saveUserInfo };
+async function insertUserFeedback(
+  user: string,
+  postedComment: string
+): Promise<void> {
+  try {
+    const userId: number = await readSingleValueFromTable<number>(
+      "id",
+      { username: user },
+      "users"
+    );
+    const datePosted: string = new Date().toISOString().split("T")[0];
+    if (userId) {
+      await insertIntoDb(
+        {
+          user_id: userId,
+          comment: postedComment,
+          date_created: datePosted,
+        },
+        "feedback"
+      );
+    } else {
+      console.error("Error");
+      throw new Error("Error saving feedback");
+    }
+  } catch (error) {
+    console.error(error, "Error saving user feedback");
+  }
+}
+
+export { saveUserInfo, insertUserFeedback };

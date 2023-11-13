838ffaf50496d75b8300470d334520ac2d239bc3
Milovan <milovan.antic314@gmail.com>
2023-11-13 12:19:08 +0100

    Add method for reading feedback

--- backend/src/Game/users.ts
+++ backend/src/Game/users.ts
@@ -1,6 +1,7 @@
 import { IFeedback, IUser } from "../dao/ITables.js";
 import { insertIntoDb } from "../dao/create.util.js";
 import { readFromDb, readSingleValueFromTable } from "../dao/read.util.js";
+import { db } from "../dao/db.js";
 
 async function saveUserInfo(username: string, password: string) {
   const user: IUser = {
@@ -43,4 +44,23 @@ async function insertUserFeedback(
   }
 }
 
-export { saveUserInfo, insertUserFeedback };
+async function getUserFeedback(user?: string): Promise<IFeedback[]> {
+  try {
+    let detailedSearch: string = "";
+    if (!!user) {
+      detailedSearch = ` and u.username = $1`;
+    }
+    const sql: string = `
+    SELECT f.comment, f.date_created, u.username
+    FROM feedback f
+    INNER JOIN users u ON f.user_id = u.id
+    ${detailedSearch};
+`;
+    const result = await db.query(sql, [user]);
+    return result.rows ? result.rows : undefined;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export { saveUserInfo, insertUserFeedback, getUserFeedback };

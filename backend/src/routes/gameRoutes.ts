4cd8f991a403f9b44459b1ea80ac985d009cd631
Milovan <milovan.antic314@gmail.com>
2024-01-20 08:20:59 +0100

    Fix imports

--- backend/src/routes/gameRoutes.ts
+++ backend/src/routes/gameRoutes.ts
@@ -1,7 +1,4 @@
-import { Request, Response } from "express";
 import express from "express";
-import { getCapitals } from "../repositories/game/capitals.js";
-import { ICapital } from "../dao/ITables.js";
 import gameControllers from "../controllers/gameControllers.js";
 
 const gameRoutes = express.Router();
